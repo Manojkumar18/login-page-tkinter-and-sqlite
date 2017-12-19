b1255694befadb97370be7147f4c4ee45b9d94cc
Manojkumar <31440315+Manojkumar18@users.noreply.github.com>
2017-12-19 14:38:07 +0200

    Update sqlitegui.py

--- sqlitegui.py
+++ sqlitegui.py
@@ -6,7 +6,7 @@ def read_db(logName,logPassword):
     if logName=='' and logPassword=='':
         print('plese enter the details')
     else:
-        line=("SELECT * FROM logindb WHERE Name= " + "'" + logName + "' ")#+"AND "+'Password ='+"'"+logPasword+"'")
+        line=("SELECT * FROM logindb WHERE Name= " + "'" + logName + "' ")
         c.execute(line)
         data =c.fetchall()
         for num in data:
